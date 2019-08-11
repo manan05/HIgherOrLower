b6c4ba197bfcd3b9dc407371b9b0cbc2d189eff6
Manan Arora <31929934+manan0313@users.noreply.github.com>
2019-08-11 13:07:22 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/higherorlower/MainActivity.java
+++ app/src/main/java/com/example/higherorlower/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnGuess = findViewById(R.id.btnGuess);
