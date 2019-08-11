f37ca3d5d82cb16f399688d64f15e339fadc7da5
Manan Arora <31929934+manan0313@users.noreply.github.com>
2019-08-11 12:43:34 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/higherorlower/MainActivity.java
+++ app/src/main/java/com/example/higherorlower/MainActivity.java
@@ -15,7 +15,11 @@ public class MainActivity extends AppCompatActivity {
     int randomNumber;
     Button btnGuess;
     EditText etNumber;
-    
+
+    public void generateRandomNos(){
+        Random rand = new Random();
+        randomNumber = rand.nextInt(20)+1;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,24 +28,30 @@ public class MainActivity extends AppCompatActivity {
         btnGuess = findViewById(R.id.btnGuess);
         etNumber = findViewById(R.id.etNumber);
 
-        Random rand = new Random();
-        randomNumber = rand.nextInt(20)+1;
+        generateRandomNos();
 
 
         btnGuess.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if(etNumber == null){
+                    etNumber.setText(null);
+                }
                 int guessValue = Integer.parseInt(etNumber.getText().toString());
                 String message;
+
                 if(guessValue > randomNumber){
                     message = "Lower !";
+                    etNumber.setText("");
                 }
                 else if (guessValue < randomNumber){
                     message = "Higher !";
+                    etNumber.setText("");
                 }
                 else{
                     message = "You got it right. GG ";
+                    etNumber.setText("");
+                    generateRandomNos();
                 }
 
                 Toast.makeText(MainActivity.this,message,Toast.LENGTH_SHORT).show();
